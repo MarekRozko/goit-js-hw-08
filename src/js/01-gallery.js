19bb310924a2102abc6129a905b6c835465a6788
Marek Rożko <maknamar12@gmail.com>
2022-11-22 17:15:00 +0100

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallaryInEl = document.querySelector('.gallery');
+const galleryMarkup = createImgGallery(galleryItems);
+gallaryInEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+function createImgGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+// Change code below this line
